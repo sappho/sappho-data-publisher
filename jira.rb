4de34bc1d38a3e1f578df4541ac2a681f1bf7035
Andrew Heald <andrew@heald.co.uk>
2012-01-06 15:35:19 +0000

    Add custom field fetch and liquid rendering.

--- jira.rb
+++ jira.rb
@@ -10,12 +10,25 @@ class Jira
   end
 
   def gather pageData, parameters
-    id = pageData[:id]
+    id = pageData['id']
     token = @jira.login @config.get('jira.username'), @config.get('jira.password')
     issue = @jira.getIssue token, id
-    summary = issue['summary']
+    pageData['page'] = issue['summary'] if !pageData['page']
+    pageData['jiraIssue'] = issue
+    customFields = {}
+    customFieldValues = issue['customFieldValues']
+    allCustomFields = @jira.getCustomFields token
+    customFieldValues.each do |customFieldValue|
+      customFieldId = customFieldValue['customfieldId']
+      allCustomFields.each do |customField|
+        customFields[customFieldId] = {
+            'name' => customField['name'],
+            'value' => customFieldValue['values'].join("\n")
+        } if customFieldId == customField['id']
+      end
+    end
+    pageData['customFields'] = customFields
     @jira.logout token
-    pageData[:page] = summary if !pageData[:page]
   end
 
 end
--- wikipublisher.rb
+++ wikipublisher.rb
@@ -1,5 +1,8 @@
+require 'rubygems'
 require 'ConfluenceWiki'
 require 'Jira'
+gem 'liquid'
+require 'liquid'
 
 class WikiPublisher
 
@@ -9,40 +12,50 @@ class WikiPublisher
 
   def publish
     config = Configuration.new
-    wiki = ConfluenceWiki.new
-    config = wiki.getPage config.get('confluence.config.space.key'), config.get('confluence.config.page.name')
-    pageData = nil
+    @wiki = ConfluenceWiki.new
     pages = []
-    config.each_line do |line|
+    getScript config.get('confluence.config.space.key'), config.get('confluence.config.page.name') do
+      |pageData| pages.push eval "{ #{pageData} }"
+    end
+    pages.each do |pageData|
+      id = pageData['id']
+      if pageData['publish']
+        report "**** Publishing #{id} ****"
+        pageData['sources'].each do |source|
+          id = source['source']
+          report "source: #{id}"
+          @@sources[id].gather pageData, source['parameters']
+        end
+        space = pageData['space']
+        pageName = pageData['page']
+        report "page: #{space}:#{pageName}"
+        template = ''
+        getScript space, pageData['template'] do
+          |templateChunk| template += templateChunk
+        end
+        template = Liquid::Template.parse template
+        report template.render 'pageData' => pageData
+      else
+        report "**** Not publishing #{id} ****"
+      end
+    end
+  end
+
+  def getScript spaceKey, pageName
+    rawPageContent = @wiki.getPage spaceKey, pageName
+    pageData = nil
+    rawPageContent.each_line do |line|
       if line =~ /^\{noformat.*\}$/
         if !pageData
           pageData = ''
         else
-          pages.push eval "{ #{pageData} }"
+          yield pageData
           pageData = nil
         end
       else
         if pageData
-          pageData = "#{pageData} #{line}"
+          pageData += line
         end
-      end
-    end
-    pages.each do |pageData|
-      id = pageData[:id]
-      if pageData[:publish]
-        report "**** Publishing #{id} ****"
-        pageData[:sources].each do |source|
-          id = source[:source]
-          report "source: #{id}"
-          @@sources[id].gather pageData, source[:parameters]
-        end
-        space = pageData[:space]
-        pageName = pageData[:page]
-        report "page: #{space}:#{pageName}"
-        template = wiki.getPage space, pageData[:template]
-
-      else
-        report "**** Not publishing #{id} ****"
       end
     end
   end
