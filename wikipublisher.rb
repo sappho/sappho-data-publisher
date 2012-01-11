79e84975cb30397f09d1468825c87bd58526d76d
Andrew Heald <andrew@heald.co.uk>
2012-01-11 12:14:36 +0000

    Tidy up.

--- wikipublisher.rb
+++ wikipublisher.rb
@@ -12,37 +12,35 @@ class WikiPublisher
     modules = Modules.instance
     config = modules.get :configuration
     logger = modules.get :logger
-    wiki = modules.get config.get 'config.wiki'
-    pages = []
-    wiki.getScript wiki.getConfiguration do |pageData|
-      hash = YAML.load pageData
-      page = hash['page']
-      pages.push page if page
-    end
-    pages.each do |pageData|
-      id = pageData['id']
-      if pageData['publish']
-        logger.report "---- publishing #{id} ----"
-        pageData['sources'].each do |source|
-          id = source['source']
-          logger.report "collecting #{id} source data"
-          modules.get(id).gatherData pageData, source['parameters']
-        end
-        pageData['publications'].each do |publication|
-          id = publication['destination']
-          logger.report "publishing data to #{id}"
-          dest = modules.get(id)
-          params = publication['parameters']
-          rawTemplate = dest.getTemplate pageData, params
-          template = ''
-          dest.getScript rawTemplate do |templateChunk|
-            template += templateChunk
+    configurator = modules.get config.get 'config.module'
+    configurator.getScript configurator.getConfiguration do |configChunk|
+      hash = YAML.load configChunk
+      pageData = hash['page']
+      if pageData
+        id = pageData['id']
+        if pageData['publish']
+          logger.report "---- publishing #{id} ----"
+          pageData['sources'].each do |source|
+            id = source['source']
+            logger.report "collecting #{id} source data"
+            modules.get(id).gatherData pageData, source['parameters']
           end
-          content = Liquid::Template.parse(template).render('data' => pageData)
-          dest.publish content, pageData, params
+          pageData['publications'].each do |publication|
+            id = publication['destination']
+            logger.report "publishing data to #{id}"
+            dest = modules.get(id)
+            params = publication['parameters']
+            rawTemplate = dest.getTemplate pageData, params
+            template = ''
+            dest.getScript rawTemplate do |templateChunk|
+              template += templateChunk
+            end
+            content = Liquid::Template.parse(template).render('data' => pageData)
+            dest.publish content, pageData, params
+          end
+        else
+          logger.report "---- not publishing #{id} ----"
         end
-      else
-        logger.report "---- not publishing #{id} ----"
       end
     end
   end
