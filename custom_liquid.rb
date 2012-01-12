26652cd579b79de74ab865b1773af6ab79d80e55
Andrew Heald <andrew@heald.co.uk>
2012-01-12 15:41:21 +0000

    Add a no-info-supplied filter.

--- custom_liquid.rb
+++ custom_liquid.rb
@@ -9,11 +9,20 @@ class CustomLiquid
 
   def CustomLiquid.setup
     Liquid::Template.register_filter(FullNameFilter)
+    Liquid::Template.register_filter(Blank)
   end
 
   module FullNameFilter
     def fullname username
       Modules.instance.get(Jira).getUser(username)['fullname']
+    end
+  end
+
+  module Blank
+    def blank text
+      text = text.join if text
+      return "This information has not been supplied." unless text and text.strip.length > 0
+      text
     end
   end
 
