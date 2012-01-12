346e720325dd64d1c4e3fb9ef0c10bb4230cc5f2
Andrew Heald <andrew@heald.co.uk>
2012-01-12 10:56:07 +0000

    A bit more logging.

--- configuration.rb
+++ configuration.rb
@@ -1,9 +1,12 @@
+require 'modules'
 require 'yaml'
 
 class Configuration
 
   def initialize
-    @config = YAML.load_file ARGV[0]
+    filename = File.expand_path(ARGV[0])
+    @config = YAML.load_file filename
+    Modules.instance.get(:logger).warn "configuration loaded from #{filename}"
   end
 
   def get key
