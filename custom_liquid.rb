81d7fdacec589ed2cd3aeb1c95184605bfc7ce27
Andrew Heald <andrew@heald.co.uk>
2012-01-12 16:43:46 +0000

    Simplify the liquid filters. There's no need to have a module per filter.

--- custom_liquid.rb
+++ custom_liquid.rb
@@ -8,22 +8,21 @@ class CustomLiquid
   Jira = 'Jira'
 
   def CustomLiquid.setup
-    Liquid::Template.register_filter(FullNameFilter)
-    Liquid::Template.register_filter(Blank)
+    Liquid::Template.register_filter(Filters)
   end
 
-  module FullNameFilter
+  module Filters
+
     def fullname username
       Modules.instance.get(Jira).getUser(username)['fullname']
     end
-  end
 
-  module Blank
     def blank text
       text = text.join if text
       return "This information has not been supplied." unless text and text.strip.length > 0
       text
     end
+
   end
 
 end
