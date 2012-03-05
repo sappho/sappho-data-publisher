c895929122381d6cbbc5a52102052f853d8ed15d
Andrew Heald <andrew@heald.co.uk>
2012-03-05 16:38:48 +0000

    Prep for release.

--- lib/sappho-data-publisher/version.rb
+++ lib/sappho-data-publisher/version.rb
@@ -7,7 +7,7 @@ module Sappho
   module Data
     module Publisher
       NAME = 'sappho-data-publisher'
-      VERSION = '0.1.7'
+      VERSION = '0.1.8'
       AUTHORS = ['Andrew Heald']
       EMAILS = ['andrew@heald.co.uk']
       HOMEPAGE = 'https://github.com/sappho/sappho-data-publisher/wiki'
