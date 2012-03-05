b129ffd5c9064bdab4b7fbba0685b3eda1e77ef4
Andrew Heald <andrew@heald.co.uk>
2012-03-05 12:32:32 +0000

    #3 - Improve the wiki chunk test.

--- test/ruby/confluence_test.rb
+++ test/ruby/confluence_test.rb
@@ -11,7 +11,7 @@ class ConfluenceTest < Test::Unit::TestCase
   def test_config_chunking
     output = ''
     @confluence.getScript File.open(testFilename('data/confluence-chunk.input'), 'rb').read do |chunk|
-      output += chunk
+      output += '----' + chunk
     end
     assert_equal File.open(testFilename('data/confluence-chunk.output'), 'rb').read, output
   end
